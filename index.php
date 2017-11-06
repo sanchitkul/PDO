e441239af97b5dcccb87adced33c53fd434093a3
Your Name <you@example.com>
2017-11-05 22:34:06 -0500

    Display Table with ID less than 6

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ define('PASSWORD', 'shaffer58');
 define('CONNECTION', 'sql2.njit.edu');
 class dbConnection
 {
-protected static $conn;
+protected static $connection;
 private function __construct()
 {
 try 
 {
-self::$conn = new PDO('mysql:host=' . CONNECTION . ';dbname=' . DATABASE, USERNAME, PASSWORD);
-self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+self::$connection = new PDO('mysql:host=' . CONNECTION . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo 'Connected successfully <br>';
 } 
 catch (PDOException $e) 
@@ -19,6 +19,65 @@ catch (PDOException $e)
 echo 'Connection failed ' . $e->getMessage() . '<br>';
 }
 }
+public static function getConnection()
+{
+if (!self::$connection)
+{
+new dbConnection();
 }
+return self::$connection;
+}
+}
+class accounts
+{
+private static $sql;
+private static $count;
+private static $recordSet;
+public static function fetchRecords()
+{
+$connection = dbConnection::getConnection();
+$tableName = get_called_class();
+$stmt = $connection->prepare('SELECT * FROM ' . $tableName . ' WHERE id < 6');
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+self::$recordSet = $stmt->fetchAll();
+}
+public static function countRecords()
+{
+self::$count = count(self::$recordSet);
+}
+public static function createTable()
+{
+if (self::$count > 0) {
+self::$sql .= '<table>';
+self::$sql .= '<tr>';
+$firstRow = self::$recordSet[0];
+foreach ($firstRow as $key => $value) {
+self::$sql .= "<th>$key</th>";
+}
+self::$sql .= '</tr>';
+foreach (self::$recordSet as $record) {
+self::$sql .= '<tr>';
+foreach ($record as $key => $value) {
+self::$sql .= "<td>$value</td>";
+}
+self::$sql .= '</tr>';
+}
+self::$sql .= '</table>';
+} 
+else 
+{
+self::$sql .= "No records";
+}
+}
+public static function printResults()
+{
 
+echo self::$sql;
+}
+}
+accounts::fetchRecords();
+accounts::countRecords();
+accounts::createTable();
+accounts::printResults();
 ?>
