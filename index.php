113ec2dd625ce11547c61794e1232a667e8c119c
Your Name <you@example.com>
2017-11-05 22:19:53 -0500

    Added statics to Connection

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
- 
- $name = 'mysql:host=sql1.njit.edu';
- $dbname = 'ssk98';
- $username = 'ssk98';
- $password = 'shaffer58';
- 
- 
- try {
- $connection = new PDO($name, $dbname, $username, $password);
- echo '<p>Connected Successfully </p>' .'<br>';
- }
- 
- catch (PDOException $e) {
- $error_message = $e -> getMessage();
- echo "<p> An error occured while connecting to the database :
- $error_message</p>".'<br>';
- }
- ?>
+define('DATABASE', 'ssk98');
+define('USERNAME', 'ssk98');
+define('PASSWORD', 'shaffer58');
+define('CONNECTION', 'sql2.njit.edu');
+class dbConnection
+{
+protected static $conn;
+private function __construct()
+{
+try 
+{
+self::$conn = new PDO('mysql:host=' . CONNECTION . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo 'Connected successfully <br>';
+} 
+catch (PDOException $e) 
+{
+echo 'Connection failed ' . $e->getMessage() . '<br>';
+}
+}
+}
+
+?>
